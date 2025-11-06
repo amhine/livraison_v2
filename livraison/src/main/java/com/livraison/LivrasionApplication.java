52916ef7202a82d34cbe4cf976e8545df0dcb897
amhine <nihadamhine@gmail.com>
2025-11-06 21:30:16 +0100

    Résolution des conflits avant push

---
+++ livraison/src/main/java/com/livraison/LivrasionApplication.java
@@ -0,0 +1,13 @@
+package com.livraison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrasionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrasionApplication.class, args);
+	}
+
+}
